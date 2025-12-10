08fa7647b832d69ae70b077f64b9af08d3b1a34c
RichardDorian <richard.dorian@outlook.com>
2025-12-10 11:07:40 +0100

    feat(db): add support for redis user password

--- backend/src/db/client.ts
+++ backend/src/db/client.ts
@@ -1,8 +1,31 @@
 import { Buffer } from 'node:buffer';
 import { createClient, RESP_TYPES } from 'redis';
+import { fromEnv } from '../utils/fromEnv.ts';
+
+const username = fromEnv('REDIS_USER', {
+  defaultValue: '',
+  warningMessage:
+    'Using default redis user. Consider changing that in production.',
+});
+
+const password = fromEnv('REDIS_PASSWORD', {
+  defaultValue: '',
+  fileExtension: true,
+  warningMessage:
+    'Redis authentication not enabled, consider using a password in production.',
+});
+
+const database = fromEnv('REDIS_DB', {
+  defaultValue: undefined,
+  type: Number,
+  allowDefaultValueInProd: true,
+});
 
 const client = createClient({
   url: 'redis://127.0.0.1:6379',
+  username,
+  password,
+  database,
 });
 
 let connected = false;
