89e8e6b498f5f9d5f300e8672933d9963f1b4cf4
RichardDorian <richard.dorian@outlook.com>
2026-01-01 09:39:53 +0100

    fix(frontend): remove uninstalled dependency in entrypoint

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,3 @@
-import * as formkit from '@formkit/vue';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import { createApp } from 'vue';
@@ -8,4 +7,4 @@ import App from './App.vue';
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
-createApp(App).use(pinia).use(formkit.plugin, formkit.defaultConfig).mount('body');
+createApp(App).use(pinia).mount('body');
